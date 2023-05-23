022bad181fb752eb9a069eb32d95f06ccff62eb9
VictorGithige <muhuthiagithige1@gmail.com>
2023-05-23 10:50:54 +0300

    initial commit

--- control_flow.rb
+++ control_flow.rb
@@ -14,3 +14,54 @@ def calculator(operation, num1, num2)
   # your code here
 end
 
+# Define a method that checks the admin login
+def admin_login(username, password)
+  if username == "admin" || username == "ADMIN" && password == "12345"
+    "Access granted"
+  else
+    "Access denied"
+  end
+end
+
+# Define a method that describes the weather based on the temperature
+def hows_the_weather(temperature)
+  if temperature < 40
+    "It's brisk out there!"
+  elsif temperature >= 40 && temperature <= 65
+    "It's a little chilly out there!"
+  elsif temperature > 85
+    "It's too dang hot out there!"
+  else
+    "It's perfect out there!"
+  end
+end
+
+# Define a method that performs the fizzbuzz operation
+def fizzbuzz(number)
+  if number % 3 == 0 && number % 5 == 0
+    "FizzBuzz"
+  elsif number % 3 == 0
+    "Fizz"
+  elsif number % 5 == 0
+    "Buzz"
+  else
+    number
+  end
+end
+
+# Define a method that performs calculations based on the operation and numbers provided
+def calculator(operation, num1, num2)
+  case operation
+  when "+"
+    num1 + num2
+  when "-"
+    num1 - num2
+  when "*"
+    num1 * num2
+  when "/"
+    num1 / num2
+  else
+    puts "Invalid operation!"
+    nil
+  end
+end
